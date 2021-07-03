24f06905d03c2958db57722a47632a9d5a396dd1
Omkar Kamale <omkar.kamale@otiselevator.com>
2021-07-03 18:16:01 +0530

    Updates

--- src/components/loader/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import loader from "./loader.gif";
-function Loader(props:any) {
-	console.log(props);
-	return <div>{props.loading && <img width="40" src={loader} alt="" />}</div>;
-}
-
-export default Loader;
