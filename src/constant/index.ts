b8dd21ebccbddcad029c1b4ed0d249b788bab3fa
Omkar Kamale <checkomkar@users.noreply.github.com>
2021-07-03 19:52:03 +0530

    some comment added

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -3,3 +3,6 @@ export enum APIKeys {
     TOKEN = "72157719485932471-daa7199c53440a20",
     SIGN = "082a2c803223e21a2d9f878476591dcb"
 }
+
+// We need to renew the KEY as it expires after a certain amout of time
+// https://www.flickr.com/services/api/explore/flickr.photos.getRecent <-- we can copy the KEY from here
